72fac260c6d13d6d5e734e965fa61a4a4383399e
jenry cacerez guzman <jenrybrown@hotmail.com>
2020-06-13 16:36:54 -0400

    Menu

--- EntityFrameworkCore/DAL/Contexto.cs
+++ EntityFrameworkCore/DAL/Contexto.cs
@@ -21,27 +21,14 @@ namespace EntityFrameworkCore.DAL
                 optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = SchoolDB; Trusted_Connection = True; ");
             }
 
-            //Ejemplo del modelBuilder
+           
             protected override void OnModelCreating(ModelBuilder modelBuilder)
             {
-                //Con estas configuraciones, fuerzo a colocar la clave primaria manualmente "Solo para la entidad de la Persona"
+                //Configurar clave primaria manualmente "Solo para la entidad de la Persona"
                 modelBuilder.Entity<Personas>().Property(p => p.personaId).HasColumnName("Id").HasDefaultValue(0).IsRequired();
 
 
-                //Relacion de uno a Varios
-                /*modelBuilder.Entity<Estudiante>()
-                    .HasOne<Grado>(e => e.Grade)
-                    .WithMany(g => g.Estudiantes) //WithOne si se quiere relacion de 1 a 1
-                    .HasForeignKey(e => e.GradoId);
-                //Para eliminar en Cascada
-                modelBuilder.Entity<Grade>()
-                    .HasMany<Estudiante>(g => g.Student)
-                    .WithOne(e => e.Grade)
-                    .HasForeignKey(e => e.GradeId)
-                    .OnDelete(DeleteBehavior.Cascade);//ClientNull si lo que se quiere es que en la otra tabla se ponga valor nulo*/
-
-                //Para Relacion varios a varios
-                //modelBuilder.Entity<Studentcourse>().HasKey(ec => new { ec.Studentid, ec.Courseid });
+             
 
 
                 modelBuilder.Entity<Estudiantes>()
@@ -49,7 +36,7 @@ namespace EntityFrameworkCore.DAL
                    .WithOne(ad => ad.Estudiantes)
                    .HasForeignKey<DireccionEstudiante>(ad => ad.direccionEstudianteId);
 
-                //Propiedad sombra
+               
                 modelBuilder.Entity<Personas>().Property<String>("Ocupacion");
                 modelBuilder.Entity<Personas>().Property<int>("Edad");
             }
--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -18,21 +18,17 @@ namespace EntityFrameworkCore
             {
                 Console.Clear();
                 Console.WriteLine(" Base de Datos");
-                Console.WriteLine("1. Guardar Estudiante en Base de Dato.");
-                Console.WriteLine("2. Hacer Query.");
-                Console.WriteLine("3. Consultar Estudiante");
-                Console.WriteLine("3. Hacer doble Query.");
-                Console.WriteLine("4. Hacer query usando SQL.");
-                Console.WriteLine("5. Actualizar  entidad.");
-                Console.WriteLine("6. Borrar data de una entidad.");
-                Console.WriteLine("7. Actualizar data  en un escenario desconectado.");
-                Console.WriteLine("8. Borrar data  en un escenario desconectado.");
-                Console.WriteLine("9. Hacer ChangeTracker a varias entidades.");
-                Console.WriteLine("10. Hacer DetachedContext a propiedades de una entidad.");
-                Console.WriteLine("11. Hacer un Graph en un escenario desconectado.");
-                Console.WriteLine("12. Hacer Querry Parametrizado.");
-                Console.WriteLine("13. Hacer doble Query.");
-                Console.WriteLine("14. Salir.");
+                Console.WriteLine("1- Guardar Estudiante en Base de Dato.");
+                Console.WriteLine("2- Hacer Query.");
+                Console.WriteLine("3- Consultar Estudiante");
+                Console.WriteLine("3- Hacer doble Query.");
+                Console.WriteLine("4- Hacer query usando SQL.");
+                Console.WriteLine("5- Actualizar  entidad.");
+                Console.WriteLine("6- Borrar data de una entidad.");
+                Console.WriteLine("7- Actualizar data  en un escenario desconectado.");
+                Console.WriteLine("8- Borrar data  en un escenario desconectado.");
+                Console.WriteLine("9- Hacer ChangeTracker.");
+                Console.WriteLine("10- Salir.");
                 Console.WriteLine("Opción:");
 
                 aux = Console.ReadLine();
@@ -95,37 +91,13 @@ namespace EntityFrameworkCore
                             Console.ReadKey();
                         }
                         break;
-                    case 10:
-                        {
-                            Tutorial.DetachedContext();
-                            Console.ReadKey();
-                        }
-                        break;
-                    case 11:
-                        {
-                            Tutorial.EntityGraphDisconnected();
-                            Console.ReadKey();
-                        }
-                        break;
-                    case 12:
-                        {
-                            Tutorial.QuerryParametrizado();
-                            Console.ReadKey();
-                        }
-                        break;
-                    case 13:
-                        {
-                            Tutorial.DoubleQuery();
-                            Console.ReadKey();
-                        }
-                        break;
-                   
+                  
                     
-                    case 14:
+                    case 10:
                         break;
                 }
             }
-            while (opc != 14);
+            while (opc != 10);
         }
 
         
